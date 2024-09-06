451a40de405aadef0be288e273f1256a10ce5236
Ayush Yadav <sidharth197502@gmail.com>
2024-09-06 20:40:42 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,73 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Notification } = require("electron");
+
+const path = require("node:path");
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // and load the index.html of the app.
+
+  mainWindow.loadFile("index.html");
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+};
+
+const NOTIFICATION_TITLE = "Basic Notification";
+const NOTIFICATION_BODY = "Notification from the Main process";
+
+function showNotification() {
+  new Notification({
+    title: NOTIFICATION_TITLE,
+    body: NOTIFICATION_BODY,
+  }).show();
+}
+//second notif
+const NOTIFICATION_TITLE2 = "Title";
+const NOTIFICATION_BODY2 =
+  "Notification from the Renderer process. Click to log to console.";
+const CLICK_MESSAGE = "Notification clicked";
+function shownotif() {
+  new Notification(NOTIFICATION_TITLE2, { body: NOTIFICATION_BODY2 }).onclick =
+    () => console.log(CLICK_MESSAGE);
+}
+
+// app.whenReady().then(createWindow).then(showNotification);
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app
+  .whenReady()
+  .then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+    showNotification();
+    shownotif();
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
