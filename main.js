5d683633532f59be03cb9200c884b6a5c3a29267
Ayush Yadav <sidharth197502@gmail.com>
2024-09-06 23:30:30 +0530

    taking screenshots, opening a base window

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Notification } = require("electron");
+const {
+  app,
+  BrowserWindow,
+  Notification,
+  BaseWindow,
+  WebContentsView,
+} = require("electron");
 
 const path = require("node:path");
+const fs = require("node:fs");
 
 const createWindow = () => {
   // Create the browser window.
@@ -10,17 +17,43 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      offscreen: true, //to remove the top options
     },
   });
 
   // and load the index.html of the app.
 
   mainWindow.loadFile("index.html");
+  // mainWindow.loadURL("https://github.com");
+
+  //dont know the meaning of base window
+  const win = new BaseWindow({
+    width: 800,
+    height: 400,
+  });
+
+  const view1 = new WebContentsView();
+  win.contentView.addChildView(view1);
+  view1.webContents.loadURL("https://www.instagram.com/");
+  view1.setBounds({ x: 100, y: 200, width: 800, height: 600 });
+
+  //to take screenshot
+  mainWindow.webContents.on("paint", (event, dirty, image) => {
+    fs.writeFileSync("example.png", image.toPNG());
+  });
+  mainWindow.webContents.setFrameRate(60);
+  console.log(
+    `The screenshot has been successfully saved to ${path.join(
+      process.cwd(),
+      "ex.png"
+    )}`
+  );
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 };
 
+//notification
 const NOTIFICATION_TITLE = "Basic Notification";
 const NOTIFICATION_BODY = "Notification from the Main process";
 
@@ -36,11 +69,11 @@ const NOTIFICATION_BODY2 =
   "Notification from the Renderer process. Click to log to console.";
 const CLICK_MESSAGE = "Notification clicked";
 function shownotif() {
-  new Notification(NOTIFICATION_TITLE2, { body: NOTIFICATION_BODY2 }).onclick =
-    () => console.log(CLICK_MESSAGE);
+  new Notification({
+    NOTIFICATION_TITLE2,
+    body: NOTIFICATION_BODY2,
+  }).show().onclick = () => console.log(CLICK_MESSAGE);
 }
-
-// app.whenReady().then(createWindow).then(showNotification);
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -68,6 +101,3 @@ app
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
